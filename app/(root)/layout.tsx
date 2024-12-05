1a6ad5a0b0d94776a8dbc924cf5e5801be0c7a4d
rk11machaba <68669221+rk11machaba@users.noreply.github.com>
2024-12-05 06:44:38 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout = ({ children}: { children: ReactNode}) => {
+  return (
+    <main>
+        Navbar
+        { children }
+        Footer
+    </main>
+  )
+}
+ export default RootLayout
