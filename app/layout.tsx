819147003550d9a8be7ad35c58ae7f052a66290d
rk11machaba <68669221+rk11machaba@users.noreply.github.com>
2024-12-10 07:44:29 +0200

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.className} ${geistMono.variable} antialiased bg-dark-2`}
-      >
-        {children}
-      </body>
+      <ClerkProvider>
+        <body
+          className={`${geistSans.className} ${geistMono.variable} antialiased bg-dark-2`}
+        >
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
